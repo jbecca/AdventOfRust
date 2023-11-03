2e21db6fb44867e4eb608b74b5a577240644c572
Jeffrey Becca <jeff.becca@protonmail.com>
2023-11-02 21:17:40 -0400

    2015 day 4 solution
    The answer really is just brute force it

---
+++ src/bin/04.rs
@@ -0,0 +1,41 @@
+pub fn part1() {
+    let mut found = false;
+    let mut num = 1;
+
+    while !found {
+        let t = "yzbqklnj".to_string() + &num.to_string();
+        let result = md5::compute(t.as_bytes());
+        let test = format!("{:x}", result);
+        if &test[0..5] == "00000" {
+            println!("{}", num);
+            found = true;
+        }
+        num += 1;
+        if num % 100000 == 0 {
+            println!("number of iters: {}", num)
+        }
+    }
+}
+pub fn part2() {
+    let mut found = false;
+    let mut num = 1;
+
+    while !found {
+        let t = "yzbqklnj".to_string() + &num.to_string();
+        let result = md5::compute(t.as_bytes());
+        let test = format!("{:x}", result);
+        if &test[0..6] == "000000" {
+            println!("{}", num);
+            found = true;
+        }
+        num += 1;
+        if num % 100000 == 0 {
+            println!("number of iters: {}", num)
+        }
+    }
+}
+
+pub fn main() {
+    part1();
+    part2();
+}
